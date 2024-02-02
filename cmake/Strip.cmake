4ebb7dc29d2ada0e04c3a3643a5cc1aa0190963e
portalplayer <portalplayer@crime.su>
2024-02-02 10:30:30 -0500

    Build Script: BaconAPI now gets stripped on release builds

--- cmake/Strip.cmake
+++ cmake/Strip.cmake
@@ -10,3 +10,9 @@ macro(ba_strip TARGET)
                        COMMAND $<$<CONFIG:release>:${CMAKE_STRIP}>
                        ARGS --strip-all $<TARGET_FILE:${TARGET}>)
 endmacro()
+
+macro(ba_strip_static TARGET)
+    if(NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC" AND CMAKE_BUILD_TYPE STREQUAL "Release")
+        target_compile_options("${TARGET}" PUBLIC -fvisibility=hidden)
+    endif()
+endmacro()
