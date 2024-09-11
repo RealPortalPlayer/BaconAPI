ae54f01bac1c656d65e725789f0cea6fbe16cf70
portalplayer <portalplayer@crime.su>
2024-09-11 12:24:02 -0400

    Number: Use types from `stdint.h`

--- include/BaconAPI/Number.h
+++ include/BaconAPI/Number.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <limits.h>
+#include <stdint.h>
 
 #include "BaconAPI/Internal/CPlusPlusSupport.h"
 #include "BaconAPI/Internal/Boolean.h"
@@ -14,32 +15,32 @@
 #define BA_NUMBER_STRING_CONVERT(type, to) type BA_Number_StringTo ## to(const char* string, char** endPointer, BA_Boolean* isError, const char* errorMessage, type defaultValue)
 
 BA_CPLUSPLUS_SUPPORT_GUARD_START()
-BA_NUMBER_STRING_CONVERT(char, Character);
-BA_NUMBER_STRING_CONVERT(unsigned char, UnsignedCharacter);
-BA_NUMBER_STRING_CONVERT(short, Short);
-BA_NUMBER_STRING_CONVERT(unsigned short, UnsignedShort);
-BA_NUMBER_STRING_CONVERT(int, Integer);
-BA_NUMBER_STRING_CONVERT(unsigned, Unsigned);
-BA_NUMBER_STRING_CONVERT(long, Long);
-BA_NUMBER_STRING_CONVERT(unsigned long, UnsignedLong);
-BA_NUMBER_STRING_CONVERT(long long, LongLong);
-BA_NUMBER_STRING_CONVERT(unsigned long long, UnsignedLongLong);
+BA_NUMBER_STRING_CONVERT(int8_t, Character);
+BA_NUMBER_STRING_CONVERT(uint8_t, UnsignedCharacter);
+BA_NUMBER_STRING_CONVERT(int16_t, Short);
+BA_NUMBER_STRING_CONVERT(uint16_t, UnsignedShort);
+BA_NUMBER_STRING_CONVERT(int32_t, Integer);
+BA_NUMBER_STRING_CONVERT(uint32_t, Unsigned);
+BA_NUMBER_STRING_CONVERT(int64_t, Long);
+BA_NUMBER_STRING_CONVERT(uint64_t, UnsignedLong);
+BA_NUMBER_STRING_CONVERT(intmax_t, LongLong);
+BA_NUMBER_STRING_CONVERT(uintmax_t, UnsignedLongLong);
 BA_CPLUSPLUS_SUPPORT_GUARD_END()
 
 #define BA_NUMBER_MIN(number, minimum) ((number) > (minimum) ? (number) : (minimum))
 #define BA_NUMBER_MAX(number, maximum) ((number) < (maximum) ? (number) : (maximum))
 #define BA_NUMBER_MIN_MAX(number, minimum, maximum) BA_NUMBER_MAX(BA_NUMBER_MIN(number, minimum), maximum)
 #define BA_NUMBER_CONVERT_INTERNAL(number, result, minimum, maximum) ((result) BA_NUMBER_MIN_MAX(number, minimum, maximum))
-#define BA_NUMBER_TO_CHARACTER(number) BA_NUMBER_CONVERT_INTERNAL(number, char, CHAR_MIN, CHAR_MAX)
-#define BA_NUMBER_TO_UNSIGNED_CHARACTER(number) BA_NUMBER_CONVERT_INTERNAL(number, unsigned char, 0, UCHAR_MAX)
-#define BA_NUMBER_TO_SHORT(number) BA_NUMBER_CONVERT_INTERNAL(number, short, SHRT_MIN, SHRT_MAX)
-#define BA_NUMBER_TO_UNSIGNED_SHORT(number) BA_NUMBER_CONVERT_INTERNAL(number, unsigned short , 0, USHRT_MAX)
-#define BA_NUMBER_TO_INTEGER(number) BA_NUMBER_CONVERT_INTERNAL(number, int, INT_MIN, INT_MAX)
-#define BA_NUMBER_TO_UNSIGNED(number) BA_NUMBER_CONVERT_INTERNAL(number, unsigned, 0, UINT_MAX)
-#define BA_NUMBER_TO_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, long, LONG_MIN, LONG_MAX)
-#define BA_NUMBER_TO_UNSIGNED_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, unsigned long, 0, ULONG_MAX)
-#define BA_NUMBER_TO_LONG_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, long long, LLONG_MIN, LLONG_MAX)
-#define BA_NUMBER_TO_UNSIGNED_LONG_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, unsigned long long, 0, ULONG_LONG_MAX)
+#define BA_NUMBER_TO_CHARACTER(number) BA_NUMBER_CONVERT_INTERNAL(number, int8_t, CHAR_MIN, CHAR_MAX)
+#define BA_NUMBER_TO_UNSIGNED_CHARACTER(number) BA_NUMBER_CONVERT_INTERNAL(number, uint8_t, 0, UCHAR_MAX)
+#define BA_NUMBER_TO_SHORT(number) BA_NUMBER_CONVERT_INTERNAL(number, int16_t, SHRT_MIN, SHRT_MAX)
+#define BA_NUMBER_TO_UNSIGNED_SHORT(number) BA_NUMBER_CONVERT_INTERNAL(number, uint16_t, 0, USHRT_MAX)
+#define BA_NUMBER_TO_INTEGER(number) BA_NUMBER_CONVERT_INTERNAL(number, int32_t, INT_MIN, INT_MAX)
+#define BA_NUMBER_TO_UNSIGNED(number) BA_NUMBER_CONVERT_INTERNAL(number, uint32_t, 0, UINT_MAX)
+#define BA_NUMBER_TO_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, int64_t, LONG_MIN, LONG_MAX)
+#define BA_NUMBER_TO_UNSIGNED_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, uint64_t, 0, ULONG_MAX)
+#define BA_NUMBER_TO_LONG_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, intmax_t, LLONG_MIN, LLONG_MAX)
+#define BA_NUMBER_TO_UNSIGNED_LONG_LONG(number) BA_NUMBER_CONVERT_INTERNAL(number, uintmax_t, 0, ULONG_LONG_MAX)
 
 #define BA_NUMBER_TO_SIGNED_CHARACTER(number) BA_NUMBER_TO_CHARACTER(number)
 #define BA_NUMBER_TO_SHORT_INTEGER(number) BA_NUMBER_TO_SHORT(number)
