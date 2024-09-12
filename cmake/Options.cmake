045858987b5b125e1ec8b595b42ee5b39f62e4c2
portalplayer <portalplayer@crime.su>
2024-09-12 11:28:07 -0400

    Build Script: Enable exports

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -4,4 +4,5 @@ macro(ba_apply_compiler_options TARGET)
                                                   "$<$<BOOL:${BA_ALLOW_DEBUG_LOGS}>:BA_ALLOW_DEBUG_LOGS>"
                                                   "$<$<BOOL:${BA_SINGLE_THREADED}>:BA_SINGLE_THREADED>")
     target_link_libraries("${TARGET}" PUBLIC "$<$<PLATFORM_ID:Windows>:dbghelp>")
+    set(CMAKE_EXECUTABLE_ENABLE_EXPORTS TRUE)
 endmacro()
