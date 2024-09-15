cfc4c9abdb11e7f0a01020e718f775e08471dbeb
portalplayer <portalplayer@crime.su>
2024-09-15 01:00:49 -0400

    Tests: Added compiler test

---
+++ test/Compiler.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#include <BaconAPI/ArgumentHandler.h>
+#include <BaconAPI/Internal/Compiler.h>
+#include <BaconAPI/Logger.h>
+
+int main(int argc, char** argv) {
+    BA_ArgumentHandler_Initialize(argc, argv);
+    BA_LOGGER_INFO("Compiler: %s\n"
+                   "Standard Version: %i\n", BA_COMPILER_NAME, BA_COMPILER_STANDARD_VERSION);
+}
