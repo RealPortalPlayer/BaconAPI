3c2d0fd1d48b1cd01db0f533545d74d69fdd103c
portalplayer <portalplayer@crime.su>
2024-09-15 10:12:39 -0400

    CompilerTest: Print compiler architecture

--- test/Compiler.c
+++ test/Compiler.c
@@ -7,6 +7,6 @@
 
 int main(int argc, char** argv) {
     BA_ArgumentHandler_Initialize(argc, argv);
-    BA_LOGGER_INFO("Compiler: %s %s\n"
-                   "Standard Version: %i\n", BA_COMPILER_NAME, BA_COMPILER_VERSION, BA_COMPILER_STANDARD_VERSION);
+    BA_LOGGER_INFO("Compiler: %s %s x%i\n"
+                   "Standard Version: %i\n", BA_COMPILER_NAME, BA_COMPILER_VERSION, BA_COMPILER_BITS, BA_COMPILER_STANDARD_VERSION);
 }
