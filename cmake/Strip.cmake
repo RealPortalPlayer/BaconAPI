007430e47386e41cadb10b47cf491722ed591285
portalplayer <portalplayer@crime.su>
2024-10-22 02:03:21 -0400

    Build Script: Strip on MinSizeRel builds

--- cmake/Strip.cmake
+++ cmake/Strip.cmake
@@ -7,12 +7,12 @@ macro(ba_strip TARGET)
     add_custom_command(TARGET "${TARGET}"
                        POST_BUILD
                        DEPENDS "${TARGET}"
-                       COMMAND $<$<OR:$<CONFIG:release>,$<CONFIG:native>>:${CMAKE_STRIP}>
+                       COMMAND $<$<OR:$<CONFIG:release>,$<CONFIG:native>,$<CONFIG:minsizerel>>:${CMAKE_STRIP}>
                        ARGS --strip-all $<TARGET_FILE:${TARGET}>)
 endmacro()
 
 macro(ba_strip_static TARGET)
     if(NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-        target_compile_options("${TARGET}" PUBLIC "$<$<OR:$<CONFIG:release>,$<CONFIG:native>>:-fvisibility=hidden>")
+        target_compile_options("${TARGET}" PUBLIC "$<$<OR:$<CONFIG:release>,$<CONFIG:native>,$<CONFIG:minsizerel>>:-fvisibility=hidden>")
     endif()
 endmacro()
