f71d6068d7500a3bade430d34c2f947dd63bde09
portalplayer <portalplayer@crime.su>
2024-11-06 13:44:50 -0500

    Translations: Added a simple preload function macro creator

--- include/BaconAPI/Translations.h
+++ include/BaconAPI/Translations.h
@@ -16,3 +16,11 @@ int BA_Translations_GetTranslationId(const char* key);
 void BA_Translations_LoadLanguage(const char* code, const char* buffer);
 void BA_Translations_Destroy(void);
 BA_CPLUSPLUS_SUPPORT_GUARD_END()
+
+#define BA_TRANSLATIONS_PRELOAD(functionName, key) \
+const char* functionName(void) {                   \
+static int translationId = -1;                 \
+if (translationId == -1)                       \
+translationId = BA_Translations_GetTranslationId(key); \
+return BA_StringManager_Get(translationId);    \
+}
